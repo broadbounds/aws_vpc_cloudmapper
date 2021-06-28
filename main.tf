05c9c2e6e6cbc96b396c28141094b2008c402ba4
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-06-27 23:23:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -149,6 +149,13 @@ resource "aws_security_group" "sg_bastion_host" {
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
    }
+ingress {
+      description = "allow cloudmapper"
+      from_port = 8000
+      to_port = 8000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+   }   
    egress {
       from_port = 0
       to_port = 0
@@ -206,6 +213,7 @@ resource "aws_instance" "bastion_host" {
    key_name = aws_key_pair.public_ssh_key.key_name
    vpc_security_group_ids = [aws_security_group.sg_bastion_host.id]
    subnet_id = aws_subnet.public_subnet.id
+   user_data = file("cloudmapper_script.sh")
    tags = {
       Name = "bastion host"
    }
