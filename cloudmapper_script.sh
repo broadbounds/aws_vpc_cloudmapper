be072601dded259e5cbb9db9fec65fbd2488e87c
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-06-28 14:07:22 -0700

    Update cloudmapper_script.sh

--- cloudmapper_script.sh
+++ cloudmapper_script.sh
@@ -9,21 +9,21 @@ sudo systemctl enable docker
 sudo git clone https://github.com/duo-labs/cloudmapper.git
 sudo cd cloudmapper
 sudo sed -i "s/urllib3==1.26.5/urllib3==1.25.10/g" requirements.txt
-sudo sed -i "s/us-east-1/$aws_region/g" Dockerfile
+sudo sed -i "s/us-east-1/${var.aws_region}/g" Dockerfile
 sudo cp config.json.demo config.json
-sudo sed -i "s/123456789012/$aws_account_id/g" config.json
-sudo sed -i "s/demo/$aws_account_name/g" config.json
+sudo sed -i "s/123456789012/${var.aws_account_id}/g" config.json
+sudo sed -i "s/demo/${var.aws_account_name}/g" config.json
 public_ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 sudo echo "CloudMapper can be accessed at $public_ip on port 8000"
 sudo echo "Report details can be accessed on /account-data/report.html"
 sudo docker build -t cloudmapper .
 sudo docker run -dt \
-        -e AWS_ACCESS_KEY_ID=$cloudmapper_access_key \
-        -e AWS_SECRET_ACCESS_KEY=$cloudmapper_secret_key \
+        -e AWS_ACCESS_KEY_ID=${var.cloudmapper_access_key} \
+        -e AWS_SECRET_ACCESS_KEY=${var.cloudmapper_secret_key} \
         -p 8000:8000 \
         --name cloudmapper \
         cloudmapper
-sudo docker exec -it cloudmapper python cloudmapper.py collect --account $aws_account_name
-sudo docker exec -it cloudmapper python cloudmapper.py report --account $aws_account_name
-sudo docker exec -it cloudmapper python cloudmapper.py prepare --account $aws_account_name
+sudo docker exec -it cloudmapper python cloudmapper.py collect --account ${var.aws_account_name}
+sudo docker exec -it cloudmapper python cloudmapper.py report --account ${var.aws_account_name}
+sudo docker exec -it cloudmapper python cloudmapper.py prepare --account ${var.aws_account_name}
 sudo docker exec -it cloudmapper python cloudmapper.py webserver --public
