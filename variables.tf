ef92f0a7e6a3813f129605d8cc089e9a31ca0301
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-06-28 20:39:51 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -33,17 +33,6 @@ variable "aws_account_id" {
   default     = ""
 }
 
-data "template_file" "cloudmapper_script" {
-  template = "${file("cloudmapper_script.tpl")}"
-  vars = {
-    cloudmapper_access_key = var.cloudmapper_access_key
-    cloudmapper_secret_key = var.cloudmapper_secret_key
-    aws_account_name = var.aws_account_name
-    aws_account_id = var.aws_account_id
-    aws_region = var.aws_region
-  }
-}
-
 variable "public_key_name" {
   type        = string
   default     = "ssh_public_key"
