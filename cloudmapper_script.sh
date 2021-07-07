21ab81b7e7cdab7999d5064e5c32041b65f7b9fb
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-06 17:25:34 -0700

    Update cloudmapper_script.sh

--- cloudmapper_script.sh
+++ cloudmapper_script.sh
@@ -28,7 +28,4 @@ docker run -dt \
         -p 8000:8000 \
         --name cloudmapper \
         cloudmapper
-docker exec -dt cloudmapper python cloudmapper.py collect --account $aws_account_name
-docker exec -dt cloudmapper python cloudmapper.py report --account $aws_account_name
-docker exec -dt cloudmapper python cloudmapper.py prepare --account $aws_account_name
-docker exec -dt cloudmapper python cloudmapper.py webserver --public
+docker exec -c "{cloudmapper python cloudmapper.py collect --account $aws_account_name} ; {cloudmapper python cloudmapper.py report --account $aws_account_name} ; {cloudmapper python cloudmapper.py prepare --account $aws_account_name} ; {cloudmapper python cloudmapper.py webserver --public}"
